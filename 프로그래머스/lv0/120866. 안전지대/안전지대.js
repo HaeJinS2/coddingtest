466e9d5b453a461c6a952de95ebdd30a1d2831c3
Haejin Jeong <56950566+HaeJinS2@users.noreply.github.com>
2023-06-28 19:21:38 +0900

    [level 0] Title: 안전지대, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -0,0 +1,25 @@
+function solution(board) {
+    var answer = 0;
+    let arr = []
+    let total = board.length * board[0].length
+
+       for(let i=0; i<board.length; i++) {
+        for(let j=0; j<board[i].length; j++) {
+            if(board[i][j] == 1) {
+                arr.push([i,j]);
+                if(i>0 && j>0) arr.push([i-1,j-1]);
+                if(i>0) arr.push([i-1,j]);
+                if(i>0 && j<board[i].length-1) arr.push([i-1,j+1]);
+                if(j>0) arr.push([i,j-1]);
+                if(j<board[i].length-1) arr.push([i,j+1]);
+                if(i<board.length-1 && j>0) arr.push([i+1,j-1]);
+                if(i<board.length-1) arr.push([i+1,j]);
+                if(i<board.length-1 && j<board[i].length-1) arr.push([i+1,j+1]);
+            }
+        }
+    }
+
+    let dangerZone = Array.from(new Set(arr.map(JSON.stringify)), JSON.parse);
+    answer = total - dangerZone.length;
+    return answer;
+}
